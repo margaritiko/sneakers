d9ebe442f0956babbc83d8435ad9163f7d5b3577
Margarita <margaritiko@gmail.com>
2020-07-10 23:15:56 +0300

    Used overriding Builder interface (@Component.Builder & @BindsInstance) 🐙

--- src/com/source/boutique/BoutiqueModule.java
+++ src/com/source/boutique/BoutiqueModule.java
@@ -4,17 +4,22 @@ import com.source.factory.Factory;
 import dagger.Module;
 import dagger.Provides;
 
+// Another way to use variable which was specified at runtime.
+// This way was replaced with overriding a Builder.
+@Deprecated
 @Module
 public class BoutiqueModule {
 
     private int numberOfBoxes;
+    private int percentageOfIncome;
 
-    public BoutiqueModule(int numberOfBoxes) {
+    public BoutiqueModule(int numberOfBoxes, int percentageOfIncome) {
         this.numberOfBoxes = numberOfBoxes;
+        this.percentageOfIncome = percentageOfIncome;
     }
 
     @Provides
     Boutique provideBoutique(Factory factory) {
-        return new Boutique(numberOfBoxes, factory);
+        return new Boutique(numberOfBoxes, percentageOfIncome, factory);
     }
 }
--- src/main/BoutiqueComponent.java
+++ src/main/BoutiqueComponent.java
@@ -1,14 +1,30 @@
 package main;
 
 import com.source.boutique.Boutique;
-import com.source.boutique.BoutiqueModule;
 import com.source.designer.logger.LoggerModule;
 import com.source.designer.service.HighFashionDesignerModule;
+import dagger.BindsInstance;
 import dagger.Component;
 
-@Component(modules = {LoggerModule.class, HighFashionDesignerModule.class, BoutiqueModule.class})
+import javax.inject.Named;
+
+@Component(modules = {LoggerModule.class, HighFashionDesignerModule.class})
 public interface BoutiqueComponent {
 
     Boutique getBoutique();
+
+    // Overriding Builder definition
+    @Component.Builder
+    interface Builder {
+
+        BoutiqueComponent build();
+
+        @BindsInstance
+        Builder numberOfBoxes(@Named("number of boxes") int numberOfBoxes);
+
+        @BindsInstance
+        Builder percentageOfIncome(@Named("percentage of income") int percentageOfIncome);
+
+    }
 
 }
